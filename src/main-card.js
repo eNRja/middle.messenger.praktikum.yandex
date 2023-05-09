52f2c89b1cd4cdbba90c915475e9300afc87c118
Алексей <satukov90@yandex.ru>
2023-05-09 20:23:51 +0300

    hot fixes 1.1

--- src/main-card.js
+++
@@ -1,9 +0,0 @@
-// import Handlebars from "handlebars";
-// import template from './home-page.hbs';
-
-// window.addEventListener('load', () => {
-//     const rootNode = document.getElementById('root');
-//     console.log(template)
-//     // const handlebarsTemplateDelegate = Handlebars.compile(template);
-//     rootNode.innerHTML = template();
-// })
