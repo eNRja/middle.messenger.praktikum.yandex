f67f8bf48e78266b83a89f24634f6d2c5dce5f10
Алексей <satukov90@yandex.ru>
2023-07-23 22:14:41 +0300

    hot_fixes

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+const PORT = process.env.PORT || 3000;
+const SOURCE = "dist";
+const INEDX_HTML = "index.html";
+
 const app = express();
-const DEFAULT_PORT = 3000;
 
-const PORT = process.env.PORT ? process.env.PORT : DEFAULT_PORT;
-
-app.use(express.static(`${__dirname}/dist`));
-const pathName = path.join(__dirname, "./dist/index.html");
-
-app.listen(PORT, () => {
-  console.log(`listening ${PORT}!`);
+app.use(express.static(path.join(__dirname, SOURCE)));
+app.use("*", (_, res) => {
+  res.sendFile(path.join(__dirname, SOURCE, INEDX_HTML));
 });
 
-app.get("/*", (_, res) => {
-  res.sendFile(pathName);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
